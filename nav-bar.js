05df0600ac2206c783aae2cb9a3c9bed3d317038
21jmoore21 <131609470+21jmoore21@users.noreply.github.com>
2024-12-30 11:38:31 -0600

    Add files via upload

--- nav-bar.js
+++ nav-bar.js
@@ -1,3 +1,17 @@
+function set_base_nav_menu() {
+    var content = "<div class='overlay-content'>" +
+    "<a onclick='setUnderline(this); aerospace_history()' id='Aerospace History'>Aerospace History<i class='arrow_right'></i></a>" +
+    "<a onclick='setUnderline(this); theory()'>Theory<i class='arrow_right'></i></a>" +
+    "<a onclick='setUnderline(this); rocket_components()'>Rocket Components<i class='arrow_right'></i></a>" +
+    "<a onclick='setUnderline(this); engineering_roles()'>Aerospace Engineering Roles<i class='arrow_right'></i></a>" +
+    "<a href='https://www.aerospacemanual.com/company_map/home.html' onclick='setUnderline(this); closeNav()'>Company Map</a>" +
+    "<a href='https://www.aerospacemanual.com/unit_conversion/home.html' onclick='setUnderline(this); closeNav()'>Unit Conversions</a>";
+
+    document.getElementById("overlay-column").innerHTML = content;
+
+    clearNav();
+}
+
 function openNav() {
     document.getElementById("myNav").style.width = "100%";
     clearNav();
