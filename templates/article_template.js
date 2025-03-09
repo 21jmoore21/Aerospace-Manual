61a1d8d0123d1fedb18ee1fa0bca4675f877f188
21jmoore21 <131609470+21jmoore21@users.noreply.github.com>
2025-03-09 14:42:24 -0500

    Add files via upload

--- templates/article_template.js
+++ templates/article_template.js
@@ -5,3 +5,19 @@ function toggleSolution(id, caretId) {
     solution.style.display = isOpen ? 'block' : 'none';
     caret.classList.toggle('open', isOpen);
 }
+
+function changeSpanText(spanId) {
+    const spanElement = document.getElementById(spanId);
+    if (spanElement.textContent == " Show Answer") {
+        spanElement.textContent = " Hide Answer";
+    } 
+    else if (spanElement.textContent == " Hide Answer"){
+        spanElement.textContent = " Show Answer";
+    }
+    else if (spanElement.textContent == " Hide Work"){
+        spanElement.textContent = " Show Work";
+    }
+    else if (spanElement.textContent == " Show Work"){
+        spanElement.textContent = " Hide Work";
+    }
+}
