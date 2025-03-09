8b516f9e656bfe7a985012faa66464b75bdeba61
21jmoore21 <131609470+21jmoore21@users.noreply.github.com>
2025-03-08 18:22:14 -0600

    Delete Templates directory

--- Templates/article_template.js
+++
@@ -1,7 +0,0 @@
-function toggleSolution(id, caretId) {
-    var solution = document.getElementById(id);
-    var caret = document.getElementById(caretId);
-    var isOpen = getComputedStyle(solution).display === 'none'; // Correct check
-    solution.style.display = isOpen ? 'block' : 'none';
-    caret.classList.toggle('open', isOpen);
-}
